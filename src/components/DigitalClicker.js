4b2a64ef62f41df640361dc352e903bc32062941
Karen V <karenvo@usc.edu>
2020-01-17 10:21:03 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -2,7 +2,26 @@
 import React from 'react';
 
 class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+    // Define the initial state
+    this.state = {
+      count: 0
+    }
+  }
+  timesClicked = () => {
+    let newCount = this.state.count + 1
+    this.setState({
+      count: newCount
+    })
+  }
 
+  render() {
+    return(
+      <h1>{this.state.count}</h1>
+      <button onClick={this.timesClicked}>Click Me!</button>
+    )
+  }
 }
 
 export default DigitalClicker;
